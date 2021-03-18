ae5f25f46988d64262b5c5d6ab254889728f0921
Aleksandr Shchekin <aleksandrshekin70@gmail.com>
2021-03-18 15:41:47 +0300

    lesson 1-9 done

--- KotlinBasic/02_Functions/app/src/main/java/com/skillbox/a02_functions/FunKotlin.kt
+++ KotlinBasic/02_Functions/app/src/main/java/com/skillbox/a02_functions/FunKotlin.kt
@@ -2,30 +2,20 @@ package com.skillbox.a02_functions
 
 import kotlin.math.sqrt
 
-
 fun main() {
 
-    println(solveEquation(a = 3, b = 2, c = 2))
+    val solutionSum = solveEquation(a = 3, b = 2, c = 4)
+    println(solutionSum)
 
 }
 
-fun  solveEquation (a: Int, b: Int, c: Int): Int {
+
+fun  solveEquation (a: Int, b: Int, c: Int): Double {
 
     val d = b * b - 4 * a * c // расчет дискриминанта
-    val sd = sqrt(3.0.toDouble())
-    val x1 = (-b + sd) / (2 * a) //расчет корня
-    val x2 = (-b - sd) / (2 * a) //расчет корня
-
-   // return x1 + x2
-
-    return if (d > 0.0) 2 else if (d < 0.0) 1 else 0
-
-    /*return when {
-        d > 0.0  -> 2
-        d < 0.0  -> 1
-        else     -> 0
-    }*/
-
+    val x1 = (-b + sqrt(d.toDouble())) / (2 * a) //расчет корня
+    val x2 = (-b - sqrt(d.toDouble())) / (2 * a) //расчет корня
+    return x1 + x2
 }
 
 
